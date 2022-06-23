456738e01ee58db16ee969771dad0c77a8352c13
codenamenam <bluetire27@gmail.com>
2022-06-24 04:38:23 +0900

    Implement Shopping Cart Context and Component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar() {
+    const { openCart, cartQuantity} = useShoppingCart()
 return <NavbarBs sticky="top" className = "bg-white shadow-sm mb-3">
     <Container>
         <Nav className="me-auto">
@@ -9,7 +11,9 @@ return <NavbarBs sticky="top" className = "bg-white shadow-sm mb-3">
             <Nav.Link to="/Store" as={NavLink}>Store</Nav.Link>
             <Nav.Link to="/About" as={NavLink}>About</Nav.Link>
         </Nav>
-        <Button style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
+        {cartQuantity >0 && (<Button
+        onClick={openCart}
+         style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
             <svg
              xmlns="http://www.w3.org/2000/svg"
              viewBox="0 0 902.86 902.86"
@@ -26,8 +30,12 @@ return <NavbarBs sticky="top" className = "bg-white shadow-sm mb-3">
 			S619.162,694.432,619.162,716.897z"/>
 
             </svg>
-            <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"}}>3</div> 
+            
+            <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"}}>
+            {cartQuantity}
+            </div> 
         </Button>
+        )} 
     </Container>
 </NavbarBs>
 
